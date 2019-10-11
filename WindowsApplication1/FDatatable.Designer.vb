b19d38793b4e367b276fd7f94c5fab35789f3bac
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2019-10-11 18:14:25 +0700

    ok

---
+++ WindowsApplication1/FDatatable.Designer.vb
@@ -0,0 +1,262 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FDatatable
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.tb_id = New System.Windows.Forms.TextBox()
+        Me.tb_jabatan = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.tb_gaji = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.tb_tanak = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.tb_tdinas = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.tb_tkesehatan = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Panel3.SuspendLayout()
+        Me.Panel4.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(22, 62)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID"
+        '
+        'tb_id
+        '
+        Me.tb_id.Location = New System.Drawing.Point(22, 79)
+        Me.tb_id.Name = "tb_id"
+        Me.tb_id.Size = New System.Drawing.Size(59, 20)
+        Me.tb_id.TabIndex = 1
+        '
+        'tb_jabatan
+        '
+        Me.tb_jabatan.Location = New System.Drawing.Point(87, 79)
+        Me.tb_jabatan.Name = "tb_jabatan"
+        Me.tb_jabatan.Size = New System.Drawing.Size(134, 20)
+        Me.tb_jabatan.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(87, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Jabatan"
+        '
+        'tb_gaji
+        '
+        Me.tb_gaji.Location = New System.Drawing.Point(22, 123)
+        Me.tb_gaji.Name = "tb_gaji"
+        Me.tb_gaji.Size = New System.Drawing.Size(98, 20)
+        Me.tb_gaji.TabIndex = 5
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(22, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 13)
+        Me.Label3.TabIndex = 4
+        Me.Label3.Text = "Gaji Pokok"
+        '
+        'tb_tanak
+        '
+        Me.tb_tanak.Location = New System.Drawing.Point(123, 123)
+        Me.tb_tanak.Name = "tb_tanak"
+        Me.tb_tanak.Size = New System.Drawing.Size(98, 20)
+        Me.tb_tanak.TabIndex = 7
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(123, 106)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(59, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Tunj. Anak"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(18, 24)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(128, 20)
+        Me.Label5.TabIndex = 8
+        Me.Label5.Text = "DATA JABATAN"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(149, 193)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(35, 23)
+        Me.Button1.TabIndex = 9
+        Me.Button1.Text = "+"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(188, 193)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(33, 23)
+        Me.Button2.TabIndex = 10
+        Me.Button2.Text = "-"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'tb_tdinas
+        '
+        Me.tb_tdinas.Location = New System.Drawing.Point(123, 167)
+        Me.tb_tdinas.Name = "tb_tdinas"
+        Me.tb_tdinas.Size = New System.Drawing.Size(98, 20)
+        Me.tb_tdinas.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(123, 150)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(61, 13)
+        Me.Label6.TabIndex = 13
+        Me.Label6.Text = "Tunj. Dinas"
+        '
+        'tb_tkesehatan
+        '
+        Me.tb_tkesehatan.Location = New System.Drawing.Point(22, 167)
+        Me.tb_tkesehatan.Name = "tb_tkesehatan"
+        Me.tb_tkesehatan.Size = New System.Drawing.Size(98, 20)
+        Me.tb_tkesehatan.TabIndex = 12
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(22, 150)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(85, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Tunj. Kesehatan"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(73, 193)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(70, 23)
+        Me.Button3.TabIndex = 15
+        Me.Button3.Text = "New"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Panel3
+        '
+        Me.Panel3.Controls.Add(Me.Label5)
+        Me.Panel3.Controls.Add(Me.Button3)
+        Me.Panel3.Controls.Add(Me.tb_id)
+        Me.Panel3.Controls.Add(Me.tb_tdinas)
+        Me.Panel3.Controls.Add(Me.Label1)
+        Me.Panel3.Controls.Add(Me.Label6)
+        Me.Panel3.Controls.Add(Me.Label2)
+        Me.Panel3.Controls.Add(Me.tb_tkesehatan)
+        Me.Panel3.Controls.Add(Me.tb_jabatan)
+        Me.Panel3.Controls.Add(Me.Label7)
+        Me.Panel3.Controls.Add(Me.Label3)
+        Me.Panel3.Controls.Add(Me.Button2)
+        Me.Panel3.Controls.Add(Me.tb_gaji)
+        Me.Panel3.Controls.Add(Me.Button1)
+        Me.Panel3.Controls.Add(Me.Label4)
+        Me.Panel3.Controls.Add(Me.tb_tanak)
+        Me.Panel3.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel3.Location = New System.Drawing.Point(0, 0)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(244, 347)
+        Me.Panel3.TabIndex = 2
+        '
+        'Panel4
+        '
+        Me.Panel4.Controls.Add(Me.DataGridView1)
+        Me.Panel4.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.Panel4.Location = New System.Drawing.Point(244, 0)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Padding = New System.Windows.Forms.Padding(10)
+        Me.Panel4.Size = New System.Drawing.Size(708, 347)
+        Me.Panel4.TabIndex = 3
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.DataGridView1.Location = New System.Drawing.Point(10, 10)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(688, 327)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'FDatatable
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(952, 347)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.Panel3)
+        Me.Name = "FDatatable"
+        Me.Text = "Form1"
+        Me.Panel3.ResumeLayout(False)
+        Me.Panel3.PerformLayout()
+        Me.Panel4.ResumeLayout(False)
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents tb_tanak As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents tb_gaji As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents tb_jabatan As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents tb_id As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents tb_tdinas As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents tb_tkesehatan As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Panel3 As System.Windows.Forms.Panel
+    Friend WithEvents Panel4 As System.Windows.Forms.Panel
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+
+End Class
---
+++ WindowsApplication1/FDatatable.vb
@@ -0,0 +1,109 @@
+﻿Imports Oracle.DataAccess.Client
+
+Public Class FDatatable
+
+    Dim dt As New DataTable
+    Dim sql, str As String
+    Dim edit As Boolean = False
+    Dim connString As String = "DATA SOURCE=DESKTOP-KEM64H6.mshome.net:1521/XE;PERSIST SECURITY INFO=True;USER ID=HENDRO; Password=orcl"
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loadGrid()
+    End Sub
+
+    Sub loadGrid()
+        Dim conn As New OracleConnection(connString) ' VB.NET
+
+        Try
+            conn.Open()
+
+            Dim d = New DataTable
+            Dim adp As New OracleDataAdapter("select * from jabatan", conn)
+            adp.Fill(d)
+
+            DataGridView1.DataSource = d
+        Catch ex As Exception ' catches any error
+            MessageBox.Show(ex.Message.ToString())
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim conn As New OracleConnection(connString) ' VB.NET
+
+        Try
+            conn.Open()
+
+            If edit = False Then
+                str = "insert into jabatan values ('" & tb_id.Text.Trim & "', '" & tb_jabatan.Text.Trim & "', " & _
+                    "" & Val(tb_gaji.Text.Trim) & ", " & Val(tb_tanak.Text.Trim) & ", " & Val(tb_tdinas.Text.Trim) & ",  " & _
+                    "" & Val(tb_tkesehatan.Text.Trim) & ")"
+            Else
+                str = "update jabatan set jabatan =  '" & tb_jabatan.Text.Trim & "', gapok =  " & Val(tb_gaji.Text.Trim) & " , " & _
+                    "tanak = " & Val(tb_tanak.Text.Trim) & ", tdinas = " & Val(tb_tdinas.Text.Trim) & ", " & _
+                    "tkesehatan = " & Val(tb_tkesehatan.Text.Trim) & " where idjabatan = '" & tb_id.Text.Trim & "'"
+            End If
+            
+            Dim cmd As New OracleCommand(str, conn)
+            cmd.ExecuteNonQuery()
+
+            loadGrid()
+            clear()
+        Catch ex As Exception ' catches any error
+            MessageBox.Show(ex.Message.ToString())
+        Finally
+            conn.Close()
+        End Try
+    End Sub
+
+    Sub clear()
+        tb_id.Text = Nothing : tb_id.Enabled = True
+        tb_jabatan.Text = Nothing
+        tb_gaji.Text = Nothing
+        tb_tanak.Text = Nothing
+        tb_tdinas.Text = Nothing
+        tb_tkesehatan.Text = Nothing
+    End Sub
+
+    Private Sub DataGridView1_CellDoubleClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellDoubleClick
+        With DataGridView1
+            Dim rowIndex As Integer = .CurrentCell.RowIndex
+            tb_id.Text = .Rows(rowIndex).Cells(0).Value
+            tb_id.Enabled = False
+            tb_jabatan.Text = .Rows(rowIndex).Cells(1).Value
+            tb_gaji.Text = .Rows(rowIndex).Cells(2).Value
+            tb_tanak.Text = .Rows(rowIndex).Cells(3).Value
+            tb_tdinas.Text = .Rows(rowIndex).Cells(4).Value
+            tb_tkesehatan.Text = .Rows(rowIndex).Cells(5).Value
+        End With
+
+        edit = True
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        clear()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        If tb_id.Text.Trim <> "" Then Exit Sub
+
+        If MsgBox("Hapus " & tb_jabatan.Text.Trim & "?", vbYesNo + vbCritical, "Perhatian") = vbYes Then
+            Dim conn As New OracleConnection(connString) ' VB.NET
+
+            Try
+                conn.Open()
+                str = "delete from jabatan where idjabatan  = '" & tb_id.Text.Trim & "'"
+                Dim cmd As New OracleCommand(str, conn)
+                cmd.ExecuteNonQuery()
+
+                loadGrid()
+                clear()
+            Catch ex As Exception ' catches any error
+                MessageBox.Show(ex.Message.ToString())
+            Finally
+                conn.Close()
+            End Try
+        End If
+    End Sub
+End Class
